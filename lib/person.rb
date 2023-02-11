b62c9b1bafc5609db43fa9c0ff708d52ab320909
+ <+>
2023-02-11 16:41:05 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
-
-end
+    # setter 
+    def name=(name)
+      @name = name 
+    end 
+    
+    # getter 
+    def name 
+      @name 
+    end 
+    
+    def job=(job)
+      @job = job
+    end 
+    
+    # getter 
+    def job 
+      @job 
+    end 
+  end
+  
+  Beyonce = Person.new 
+  Beyonce.name = "Beyonce"
+  Beyonce.job = "Singer"
+ 
+  
